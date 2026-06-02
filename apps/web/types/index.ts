9add63033cc460389f24a7eec37bc21e875f520a
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2026-06-03 02:22:38 +0600

    feat: add application utils types

---
+++ apps/web/types/index.ts
@@ -0,0 +1,65 @@
+import type { RouteType } from "next/dist/lib/load-custom-routes";
+
+import { PermissionDataModel, RoleDataModel } from "@workspace/drizzle/schemas";
+import { RoleTypeEnumType } from "@workspace/drizzle/zod-db-enums";
+
+export type AuthUser = {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  email: string;
+  emailVerified: boolean;
+  name: string;
+  image?: string | null | undefined;
+  banned: boolean | null | undefined;
+  role?: string | null | undefined;
+  banReason?: string | null | undefined;
+  banExpires?: Date | null | undefined;
+};
+
+export type AuthSession = {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  userId: string;
+  expiresAt: Date;
+  token: string;
+  ipAddress?: string | null | undefined;
+  userAgent?: string | null | undefined;
+  activeOrganizationId?: string | null | undefined;
+  activeTeamId?: string | null | undefined;
+  impersonatedBy?: string | null | undefined;
+};
+
+export interface FieldError<TFieldNames> {
+  fieldName: TFieldNames;
+  message: string;
+}
+
+export interface IApiHookInput<TFieldNames = string> {
+  onRequestStart?: () => void;
+  onRequestEnd?: () => void;
+  onSuccess?: (message: string) => void;
+  onError?: (errorMessage: string) => void;
+  onValidationErrors?: (fields: Array<FieldError<TFieldNames>>) => void;
+}
+
+export interface RoleWithContext extends Pick<
+  RoleDataModel,
+  "roleName" | "type" | "customRoleName"
+> {
+  source: RoleTypeEnumType;
+  orgId?: string;
+  orgSlug?: string;
+}
+
+export interface PermissionWithContext extends Pick<
+  PermissionDataModel,
+  "name" | "level" | "resource" | "action"
+> {
+  source: RoleTypeEnumType;
+  orgId?: string;
+  orgSlug?: string;
+}
+
+export type RoutePathType = __next_route_internal_types__.RouteImpl<RouteType>;
