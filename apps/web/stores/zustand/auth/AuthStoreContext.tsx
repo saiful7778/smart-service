71bc484909d2b9a052abc985af100f23b8cbc93f
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2026-06-03 17:33:33 +0600

    feat: add zustand store and create auth store

---
+++ apps/web/stores/zustand/auth/AuthStoreContext.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+import { StoreApi, useStore } from "zustand";
+
+import type {
+  AuthSession,
+  AuthUser,
+  PermissionWithContext,
+  RoleWithContext,
+} from "@/types";
+
+import { authStore, AuthStoreAction, AuthStoreState } from "./authStore";
+
+const AuthStoreContext = createContext<StoreApi<
+  AuthStoreState & AuthStoreAction
+> | null>(null);
+
+interface AuthStoreProviderProps extends React.PropsWithChildren {
+  user: AuthUser;
+  session: AuthSession;
+  roles: Array<RoleWithContext>;
+  permissions: Array<PermissionWithContext>;
+}
+
+export function AuthStoreProvider({
+  children,
+  user,
+  session,
+  roles,
+  permissions,
+}: AuthStoreProviderProps) {
+  const [store] = useState<StoreApi<AuthStoreState & AuthStoreAction>>(() =>
+    authStore(user, session, roles, permissions)
+  );
+
+  return (
+    <AuthStoreContext.Provider value={store}>
+      {children}
+    </AuthStoreContext.Provider>
+  );
+}
+
+export function useAuthStore<T>(
+  selector: (state: AuthStoreState & AuthStoreAction) => T
+) {
+  const store = useContext(AuthStoreContext);
+  if (!store) {
+    throw new Error("AuthStoreProvider is not found");
+  }
+  return useStore(store, selector);
+}
---
+++ apps/web/stores/zustand/auth/authStore.ts
@@ -0,0 +1,59 @@
+import { createStore } from "zustand";
+import { combine, devtools } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
+
+import { env } from "@/lib/env";
+
+import {
+  AuthSession,
+  AuthUser,
+  PermissionWithContext,
+  RoleWithContext,
+} from "@/types";
+
+export interface AuthStoreState {
+  user: AuthUser;
+  session: AuthSession;
+  roles: Array<RoleWithContext>;
+  permissions: Array<PermissionWithContext>;
+}
+
+export interface AuthStoreAction {
+  addUserData: (userData: AuthUser) => void;
+}
+
+export function authStore(
+  userData: AuthUser,
+  sessionData: AuthSession,
+  roles: Array<RoleWithContext>,
+  permissions: Array<PermissionWithContext>
+) {
+  return createStore<AuthStoreState & AuthStoreAction>()(
+    devtools(
+      immer(
+        combine<AuthStoreState, AuthStoreAction>(
+          {
+            user: userData,
+            session: sessionData,
+            roles,
+            permissions,
+          },
+          (set) => ({
+            addUserData: (userData) => {
+              set((state) => {
+                state.user = userData;
+                return state;
+              });
+            },
+          })
+        )
+      ),
+
+      {
+        name: "auth-store",
+        store: "auth-store",
+        enabled: env.NEXT_PUBLIC_NODE_ENV === "development",
+      }
+    )
+  );
+}
