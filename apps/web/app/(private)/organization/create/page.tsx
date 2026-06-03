18f96c477a8a9671b8da52cb11f8aba7e72a31b9
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2026-06-03 23:12:23 +0600

    feat: add org create page

---
+++ apps/web/app/(private)/organization/create/page.tsx
@@ -0,0 +1,26 @@
+import { redirect } from "next/navigation";
+
+import { CreateOrgForm } from "@/features/org/components/forms/CreateOrgForm";
+import type { RoutePathType } from "@/types";
+
+export default async function OrgCreatePage(
+  props: PageProps<"/organization/create">
+) {
+  const searchParams = await props.searchParams;
+  const invitationId = searchParams?.invitationId as string | undefined;
+
+  if (invitationId) {
+    redirect(
+      `/organization/accept-invitation?invitationId=${invitationId}` as RoutePathType
+    );
+  }
+
+  return (
+    <div className="flex items-center justify-center min-h-svh bg-muted/50 py-4">
+      <div className="max-w-3xl w-full bg-background shadow-lg rounded-lg p-4 border">
+        <h1 className="text-2xl font-bold text-center">Create Organization</h1>
+        <CreateOrgForm />
+      </div>
+    </div>
+  );
+}
