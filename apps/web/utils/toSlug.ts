b3866e0d6b695179863449833a7c730f8887eb24
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2026-06-03 22:59:54 +0600

    feat: add string to slug convater

---
+++ apps/web/utils/toSlug.ts
@@ -0,0 +1,9 @@
+export function toSlug(value: string): string {
+  return value
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9\s-]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-")
+    .replace(/^-|-$/g, "");
+}
