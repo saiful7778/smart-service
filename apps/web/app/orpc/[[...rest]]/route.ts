7c2fc7fe234dd8fe7e62f87888231cf644ed389d
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2026-06-03 14:37:02 +0600

    feat: intregate orpc

---
+++ apps/web/app/orpc/[[...rest]]/route.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { db } from "@/lib/db";
+import { logger } from "@/lib/logger";
+import { supabaseServerClient } from "@/lib/supabase/server-client";
+
+import { rpcHandler } from "@/server/orpc.handler";
+
+export const runtime = "nodejs";
+
+async function handleRequest(request: NextRequest) {
+  const { response } = await rpcHandler.handle(request, {
+    prefix: "/orpc",
+    context: {
+      reqHeaders: request.headers,
+      db,
+      logger,
+      user: null,
+      session: null,
+      roles: null,
+      permissions: null,
+      supabaseClient: supabaseServerClient,
+    },
+  });
+
+  return (
+    response ??
+    NextResponse.json(
+      {
+        defined: true,
+        code: "NOT_FOUND",
+        status: 404,
+        message: `'${request.url}' - is not found`,
+      },
+      { status: 404 }
+    )
+  );
+}
+
+export const HEAD = handleRequest;
+export const GET = handleRequest;
+export const POST = handleRequest;
+export const PUT = handleRequest;
+export const PATCH = handleRequest;
+export const DELETE = handleRequest;
