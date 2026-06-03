9600a5db516bc32d021fbe332572ab591dadcb91
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2026-06-03 22:59:21 +0600

    feat: add dev and prod image host server support

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -9,6 +9,41 @@ const nextConfig: NextConfig = {
   ],
   typedRoutes: true,
   reactCompiler: true,
+  images: {
+    dangerouslyAllowLocalIP: process.env.NODE_ENV !== "production",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.dicebear.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.jsdelivr.net",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "tiobqbatrxtpmsletbcc.storage.supabase.co",
+        port: "",
+        pathname: "/storage/v1/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "54321",
+        pathname: "/storage/v1/**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "54321",
+        pathname: "/storage/v1/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
