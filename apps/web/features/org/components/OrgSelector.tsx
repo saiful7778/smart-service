9215768256a04220b970861bfd08f53e0236942e
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2026-06-03 19:38:41 +0600

    feat: add org selector

---
+++ apps/web/features/org/components/OrgSelector.tsx
@@ -0,0 +1,155 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+import { ChevronsUpDown } from "lucide-react";
+import toast from "react-hot-toast";
+
+import { Badge } from "@workspace/ui/components/badge";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@workspace/ui/components/dropdown-menu";
+import {
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@workspace/ui/components/sidebar";
+
+import { authClient } from "@/lib/better-auth/auth-client";
+
+import orgPlaceholderImg from "@/public/org_placeholder.png";
+import { StateOrganizationType } from "@/stores/zustand/org/orgStore";
+import { useOrgStore } from "@/stores/zustand/org/OrgStoreContext";
+import { resolveImagePath } from "@/utils/resolveImagePath";
+
+export function OrgSelector() {
+  const { isMobile } = useSidebar();
+  const orgs = useOrgStore((state) => state.organizations);
+  const activeOrg = useOrgStore((state) => state.activeOrg);
+  const setActiveOrg = useOrgStore((state) => state.setActiveOrg);
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleSelect = async (org: StateOrganizationType) => {
+    const toastId = "set_active_org_toast_message";
+
+    if (org.id === activeOrg?.id) {
+      toast.success("Organization is already active!");
+      return;
+    }
+
+    return authClient.organization.setActive({
+      organizationId: org.id,
+      organizationSlug: org.slug,
+      fetchOptions: {
+        onRequest: () => {
+          setIsLoading(true);
+          toast.loading("Loading...", { id: toastId });
+        },
+        onSuccess: async () => {
+          setIsLoading(false);
+          toast.success("Organization loaded", {
+            id: toastId,
+          });
+          setActiveOrg(org);
+        },
+        onError: ({ error }) => {
+          setIsLoading(false);
+          toast.error(error.message ?? "Failed to load organization.", {
+            id: toastId,
+          });
+        },
+      },
+    });
+  };
+
+  if (orgs.length === 0) return null;
+
+  return (
+    <DropdownMenu>
+      <SidebarMenuItem>
+        <DropdownMenuTrigger
+          disabled={isLoading}
+          render={
+            <SidebarMenuButton className="rounded-md border" size="lg">
+              {activeOrg ? (
+                <>
+                  <span className="inline-flex aspect-square size-8 items-center justify-center">
+                    <span className="overflow-hidden shrink-0 size-full rounded-lg border">
+                      <Image
+                        src={
+                          activeOrg.logo
+                            ? resolveImagePath(activeOrg.logo)
+                            : orgPlaceholderImg.src
+                        }
+                        className="size-full object-cover object-center"
+                        alt={`${activeOrg.name} logo`}
+                        width={36}
+                        height={36}
+                      />
+                    </span>
+                  </span>
+                  <span className="grid flex-1 text-left leading-tight">
+                    <span className="truncate font-medium text-sm">
+                      {activeOrg.name}
+                    </span>
+                    <span className="truncate text-xs text-muted-foreground">
+                      {activeOrg.slug}
+                    </span>
+                  </span>
+                  <ChevronsUpDown className="ml-auto" />
+                </>
+              ) : (
+                "Organization is unselected"
+              )}
+            </SidebarMenuButton>
+          }
+        >
+          Open organization selector
+        </DropdownMenuTrigger>
+      </SidebarMenuItem>
+
+      <DropdownMenuContent
+        className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+        align="start"
+        side={isMobile ? "bottom" : "right"}
+        sideOffset={4}
+      >
+        <DropdownMenuGroup>
+          <DropdownMenuLabel>Organizations</DropdownMenuLabel>
+          {orgs.map((org) => (
+            <DropdownMenuItem
+              disabled={isLoading}
+              key={org.name}
+              onClick={() => handleSelect(org)}
+            >
+              <div className="inline-flex aspect-square size-7 overflow-hidden items-center justify-center rounded-md border">
+                <Image
+                  src={
+                    org.logo
+                      ? resolveImagePath(org.logo)
+                      : orgPlaceholderImg.src
+                  }
+                  className="size-full shrink-0 object-cover object-center"
+                  alt={`${org.name} logo`}
+                  width={36}
+                  height={36}
+                />
+              </div>
+              <span>{org.name}</span>
+              {org.id === activeOrg?.id && (
+                <Badge variant="outline">Active</Badge>
+              )}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
