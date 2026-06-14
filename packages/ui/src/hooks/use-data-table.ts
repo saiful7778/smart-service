f9a28e9aa156c3eaf83bfa0445027eba6338fbf8
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2026-06-15 03:50:39 +0600

    feat: add tanstack table custom hook

---
+++ packages/ui/src/hooks/use-data-table.ts
@@ -0,0 +1,228 @@
+"use client";
+
+import { useCallback, useMemo, useState } from "react";
+
+import type {
+  ColumnFiltersState,
+  PaginationState,
+  RowSelectionState,
+  SortingState,
+  TableOptions,
+  Updater,
+  VisibilityState,
+} from "@tanstack/react-table";
+import {
+  getCoreRowModel,
+  getFacetedMinMaxValues,
+  getFacetedRowModel,
+  getFacetedUniqueValues,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import { useDebouncedCallback } from "@workspace/ui/hooks/use-debounced-callback";
+import type { FiltersType } from "@workspace/ui/types/data-table.js";
+
+interface UseDataTableProps<TData>
+  extends
+    Omit<
+      TableOptions<TData>,
+      | "state"
+      | "pageCount"
+      | "getCoreRowModel"
+      | "manualFiltering"
+      | "manualPagination"
+      | "manualSorting"
+    >,
+    Required<Pick<TableOptions<TData>, "pageCount">> {
+  enableAdvancedFilter?: boolean;
+  filters: Omit<FiltersType, "search">;
+  setFilters: (filters: Omit<FiltersType, "search">) => void;
+}
+
+export const DEFAULT_PAGE_INDEX = 1;
+export const DEFAULT_PAGE_SIZE = 20;
+
+export function useDataTable<TData>({
+  columns,
+  filters,
+  setFilters,
+  pageCount = -1,
+  enableAdvancedFilter = false,
+  ...props
+}: UseDataTableProps<TData>) {
+  "use no memo";
+  const [rowSelection, setRowSelection] = useState<RowSelectionState>({});
+
+  const {
+    page = DEFAULT_PAGE_INDEX,
+    limit = DEFAULT_PAGE_SIZE,
+    order,
+    orderField,
+    filter,
+  } = filters;
+
+  const pagination: PaginationState = useMemo(() => {
+    return {
+      pageIndex: page - 1, // zero-based index -> one-based index
+      pageSize: limit,
+    };
+  }, [page, limit]);
+
+  const onPaginationChange = useCallback(
+    (updaterOrValue: Updater<PaginationState>) => {
+      const next =
+        typeof updaterOrValue === "function"
+          ? updaterOrValue(pagination)
+          : updaterOrValue;
+
+      void setFilters({
+        page: next.pageIndex + 1,
+        limit: next.pageSize,
+      });
+    },
+    [pagination, setFilters]
+  );
+
+  const sorting: SortingState = useMemo(() => {
+    if (!orderField || !order) return [];
+    return [{ id: orderField, desc: order === "desc" ? true : false }];
+  }, [orderField, order]);
+
+  const onSortingChange = useCallback(
+    (updaterOrValue: Updater<SortingState>) => {
+      const next =
+        typeof updaterOrValue === "function"
+          ? updaterOrValue(sorting)
+          : updaterOrValue;
+
+      if (next.length > 0) {
+        void setFilters({
+          orderField: next[0]!.id,
+          order: next[0]!.desc ? "desc" : "asc",
+        });
+      } else {
+        void setFilters({
+          orderField: undefined,
+          order: undefined,
+        });
+      }
+    },
+    [sorting, setFilters]
+  );
+
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+
+  const filterableColumns = useMemo(() => {
+    if (enableAdvancedFilter) return [];
+
+    return columns.filter((column) => column.enableColumnFilter);
+  }, [columns, enableAdvancedFilter]);
+
+  const initialColumnFilters: ColumnFiltersState = useMemo(() => {
+    if (enableAdvancedFilter) return [];
+
+    return Object.entries(filter ?? {}).reduce<ColumnFiltersState>(
+      (filters, [key, value]) => {
+        if (value !== null) {
+          const processedValue = Array.isArray(value)
+            ? value
+            : typeof value === "string" && /[^a-zA-Z0-9]/.test(value)
+              ? value.split(",").filter(Boolean)
+              : [value];
+
+          filters.push({
+            id: key,
+            value: processedValue,
+          });
+        }
+        return filters;
+      },
+      []
+    );
+  }, [filter, enableAdvancedFilter]);
+
+  const columnFilters: ColumnFiltersState = useMemo(() => {
+    if (enableAdvancedFilter) return [];
+
+    return initialColumnFilters;
+  }, [initialColumnFilters, enableAdvancedFilter]);
+
+  const debouncedSetFilterValues = useDebouncedCallback(
+    (values: typeof filter) => {
+      void setFilters({
+        page: DEFAULT_PAGE_INDEX,
+        filter: values,
+      });
+    },
+    300
+  );
+
+  const onColumnFiltersChange = useCallback(
+    (updaterOrValue: Updater<ColumnFiltersState>) => {
+      if (enableAdvancedFilter) return;
+      const next =
+        typeof updaterOrValue === "function"
+          ? updaterOrValue(columnFilters)
+          : updaterOrValue;
+
+      const filterUpdates = next.reduce<
+        Record<string, string | string[] | null>
+      >((acc, filter) => {
+        if (filterableColumns.find((column) => column.id === filter.id)) {
+          acc[filter.id] = filter.value as string | string[] | null;
+        }
+        return acc;
+      }, {});
+
+      for (const prevFilter of columnFilters) {
+        if (!next.some((filter) => filter.id === prevFilter.id)) {
+          filterUpdates[prevFilter.id] = null;
+        }
+      }
+
+      debouncedSetFilterValues(filterUpdates);
+    },
+    [
+      columnFilters,
+      enableAdvancedFilter,
+      filterableColumns,
+      debouncedSetFilterValues,
+    ]
+  );
+
+  // eslint-disable-next-line react-hooks/incompatible-library
+  return useReactTable({
+    ...props,
+    pageCount,
+    columns,
+    state: {
+      pagination,
+      sorting,
+      columnVisibility,
+      rowSelection,
+      columnFilters,
+    },
+    defaultColumn: {
+      ...props.defaultColumn,
+      enableColumnFilter: false,
+    },
+    onRowSelectionChange: setRowSelection,
+    onPaginationChange,
+    onSortingChange,
+    onColumnFiltersChange,
+    onColumnVisibilityChange: setColumnVisibility,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFacetedRowModel: getFacetedRowModel(),
+    getFacetedUniqueValues: getFacetedUniqueValues(),
+    getFacetedMinMaxValues: getFacetedMinMaxValues(),
+    manualPagination: true,
+    manualSorting: true,
+    manualFiltering: true,
+  });
+}
