9734b286845b92a2e7d71795dbd46885d7bc94e7
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2026-08-09 22:22:36 +0600

    feat(eslint): integrate typescript eslint recommendations

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -4,6 +4,7 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import pluginReact from "eslint-plugin-react";
 import pluginReactHooks from "eslint-plugin-react-hooks";
 import globals from "globals";
+import tseslint from "typescript-eslint";
 
 import { config as baseConfig } from "./base.js";
 
@@ -16,11 +17,15 @@ export const nextJsConfig = [
   ...baseConfig,
   js.configs.recommended,
   eslintConfigPrettier,
+  ...tseslint.configs.recommended,
   {
     ...pluginReact.configs.flat.recommended,
     languageOptions: {
       ...pluginReact.configs.flat.recommended.languageOptions,
       globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
         ...globals.serviceworker,
       },
     },
