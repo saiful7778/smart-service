c4823ac4f32b95b542fe19b2068b223071a6b340
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2026-08-13 01:41:00 +0600

    feat(skeleton): use new data table skeleton component

--- apps/web/features/user/components/user-table/UserManagementTable.tsx
+++ apps/web/features/user/components/user-table/UserManagementTable.tsx
@@ -4,7 +4,8 @@ import { useQuery } from "@tanstack/react-query";
 
 import { DataTableEmpty } from "@workspace/ui/components/data-table/data-table-empty";
 import { DataTableGlobalSearch } from "@workspace/ui/components/data-table/data-table-global-search";
-import { DataTableSkeleton } from "@workspace/ui/components/data-table/data-table-skeleton";
+import { DataTableSkeleton } from "@workspace/ui/components/data-table/DataTableSkeleton";
+import { DataTableToolbarSkeleton } from "@workspace/ui/components/data-table/DataTableToolbarSkeleton";
 import { useDebouncedCallback } from "@workspace/ui/hooks/use-debounced-callback";
 
 import { QueryStateBoundary } from "@/lib/tanstack/query/QueryStateBoundary";
@@ -69,7 +70,11 @@ export function UserManagementTable({
         error={error}
         data={data?.data}
         isEmpty={() => false}
-        loadingFallback={<DataTableSkeleton />}
+        loadingFallback={
+          <DataTableSkeleton>
+            <DataTableToolbarSkeleton />
+          </DataTableSkeleton>
+        }
         emptyFallback={<DataTableEmpty />}
       >
         {(data) => (
