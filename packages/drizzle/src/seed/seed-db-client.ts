6e8b2e13a35d243cedebdd90ff2e9c9fd85c96b6
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2026-08-14 13:02:34 +0600

    feat(env): consolidate environment configuration files and update docker-compose

--- packages/drizzle/src/seed/seed-db-client.ts
+++ packages/drizzle/src/seed/seed-db-client.ts
@@ -4,13 +4,8 @@ import { config } from "dotenv";
 
 import { createDrizzleClient } from "../drizzle-client";
 
-const NODE_ENV = process.env.NODE_ENV || "development";
-
 config({
-  path: [
-    join(process.cwd(), "../../.env"),
-    join(process.cwd(), `../../.env.${NODE_ENV}.local`),
-  ],
+  path: [join(process.cwd(), "../../.env")],
 });
 
 export const db = createDrizzleClient({
